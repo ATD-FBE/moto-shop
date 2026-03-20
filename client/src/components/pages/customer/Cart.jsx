7e38347410a8c362fe2b38094a5f4d8b37f8e6cf
ATD <atd.fbe@yandex.com>
2026-03-20 12:32:02 +0300

    Refactor: component mini-fix

--- client/src/components/pages/customer/Cart.jsx
+++ client/src/components/pages/customer/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo, useEffect } from 'react';
+import { useState, useRef, useMemo, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import cn from 'classnames';
